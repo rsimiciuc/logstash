c6c852a83d4f25ff0f3ab576fbda6bc4ec0c1f58
Raul Simiciuc <byraul@gmail.com>
2014-04-16 15:10:07 +0100

    catching exception

--- lib/logstash/event.rb
+++ lib/logstash/event.rb
@@ -31,7 +31,7 @@ end
 # * "@version" - the version of the schema. Currently "1"
 #
 # They are prefixed with an "@" symbol to avoid clashing with your
-# own custom fields. 
+# own custom fields.
 #
 # When serialized, this is represented in JSON. For example:
 #
@@ -54,7 +54,7 @@ class LogStash::Event
 
     @data = data
     data[VERSION] = VERSION_ONE if !@data.include?(VERSION)
-    if data.include?(TIMESTAMP) 
+    if data.include?(TIMESTAMP)
       t = data[TIMESTAMP]
       if t.is_a?(String)
         data[TIMESTAMP] = LogStash::Time.parse_iso8601(t)
@@ -113,7 +113,7 @@ class LogStash::Event
   def ruby_timestamp
     raise DeprecatedMethod
   end # def unix_timestamp
-  
+
   # field-related access
   public
   def [](str)
@@ -123,7 +123,7 @@ class LogStash::Event
       return LogStash::Util::FieldReference.exec(str, @data)
     end
   end # def []
-  
+
   public
   def []=(str, value)
     if str == TIMESTAMP && !value.is_a?(Time)
@@ -162,12 +162,12 @@ class LogStash::Event
   def fields
     raise DeprecatedMethod
   end
-  
+
   public
   def to_json(*args)
     begin
         return @data.to_json(*args)
-    rescue GeneratorError
+    rescue => ex
         return JSON.parse("{\"logstash-error\":\"Unrecognized character in line\"}")
     end
   end # def to_json
@@ -203,7 +203,7 @@ class LogStash::Event
   end # def remove
 
   # sprintf. This could use a better method name.
-  # The idea is to take an event and convert it to a string based on 
+  # The idea is to take an event and convert it to a string based on
   # any format values, delimited by %{foo} where 'foo' is a field or
   # metadata member.
   #
@@ -216,7 +216,7 @@ class LogStash::Event
   # If a %{name} value is an array, then we will join by ','
   # If a %{name} value does not exist, then no substitution occurs.
   #
-  # TODO(sissel): It is not clear what the value of a field that 
+  # TODO(sissel): It is not clear what the value of a field that
   # is an array (or hash?) should be. Join by comma? Something else?
   public
   def sprintf(format)
